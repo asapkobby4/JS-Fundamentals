e4bf85005ec46c28e77e1f2e49f332314a622701
asapkobby4 <158980952+asapkobby4@users.noreply.github.com>
2025-06-01 13:32:56 -0400

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,7 @@
 const arg = process.argv[2];
 const num = parseint(arg);
-
-if (Isnan(num)) {
-  console.log('Not a number');
-} else {
+if (!Isnan(num)) {
   console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
 }
